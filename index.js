22da3446a64bbc02629d7101f2beae361fb266a1
lauramgr <56912363+lauramgr@users.noreply.github.com>
2019-12-16 15:10:43 +0100

    Router Version 1.1
    Alle Seiten wurden im Router mit den Pfaden angelegt.
    
    Es müssen nun die Seiten unter "views" angelegt und mit Code gefüllt werden

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Startseite'
 import Navbar from '/components/Navbar.vue'
+import Profilseite from ".../views/Profilseite"
+import Profilseite_Artist from ".../views/Profilseite_Artist"
+import Profilseite_Studio from ".../views/Profilseite_Studio"
+import Choose_your_Role from ".../views/Choose_your_Role"
+import Datenschutzbestimmungen from ".../views/Datenschutzbestimmungen"
+import Impressum from ".../views/Impressum"
 
 
 Vue.use(VueRouter)
@@ -15,8 +20,30 @@ export default new VueRouter({
         },
 
         {
-            path: '/Startseite.vue',
-            component: Login
+            path: '/Profilseite.vue',
+            component: Profilseite
+
+        },
+        {
+            path: '/Profilseite_Artist.vue',
+            component: Profilseite_Artist
+
+        },
+        {
+            path: '/Profilseite_Studio.vue',
+            component: Profilseite_Studio
+        },
+        {
+            path: '/Choose_your_Role.vue',
+            component: Choose_your_Role
+        },
+        {
+            path: '/Datenschutzbestimmungen.vue',
+            component: Datenschutzbestimmungen
+        },
+        {
+            path: '/Impressum.vue',
+            component: Impressum
         },
     ]
 })
