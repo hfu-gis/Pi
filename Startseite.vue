4ade7998f76b4a52f4195ba90a65b2be04a5bbfe
lauramgr <56912363+lauramgr@users.noreply.github.com>
2019-12-30 21:24:49 +0100

    Startseite 1.3 und Router Update

---
+++ Startseite.vue
@@ -0,0 +1,25 @@
+<template>
+
+        <v-app-bar app right dark
+                   src=background color="dark grey">
+            <v-toolbar-title>SESSIONS</v-toolbar-title>
+            <v-spacer />
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon :to="{name:'Profilseite'}">
+                <v-icon>mdi-account</v-icon>
+            </v-btn>
+
+            <v-btn v-if="!isLoggedIn" icon :to="{name:'Login'}">
+                <v-icon>mdi-login</v-icon>
+            </v-btn>
+
+        </v-app-bar>
+
+
+
+
+</template>
+
--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Navbar from '/components/Navbar.vue'
-import Profilseite from ".../views/Profilseite"
-import Profilseite_Artist from ".../views/Profilseite_Artist"
-import Profilseite_Studio from ".../views/Profilseite_Studio"
-import Choose_your_Role from ".../views/Choose_your_Role"
-import Datenschutzbestimmungen from ".../views/Datenschutzbestimmungen"
-import Impressum from ".../views/Impressum"
+import Login from '../views/Login'
+import HelloWorld from "../components/HelloWorld";
+import Startseite from '../views/Startseite'
 
 
 Vue.use(VueRouter)
@@ -14,36 +10,20 @@ Vue.use(VueRouter)
 export default new VueRouter({
     routes: [
         {
-            path: '/Startseite.vue',
-            component: Navbar
-
-        },
-
-        {
-            path: '/Profilseite.vue',
-            component: Profilseite
-
+            path: '/Startseite',
+            component: Startseite
         },
         {
-            path: '/Profilseite_Artist.vue',
-            component: Profilseite_Artist
-
+            path: '/login',
+            component: Login
         },
         {
-            path: '/Profilseite_Studio.vue',
-            component: Profilseite_Studio
+            path: '/login',
+            component: Login
         },
         {
-            path: '/Choose_your_Role.vue',
-            component: Choose_your_Role
-        },
-        {
-            path: '/Datenschutzbestimmungen.vue',
-            component: Datenschutzbestimmungen
-        },
-        {
-            path: '/Impressum.vue',
-            component: Impressum
+            path: '/login',
+            component: Login
         },
     ]
 })
