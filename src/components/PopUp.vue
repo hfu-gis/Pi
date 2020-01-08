6864fc35660c72efb48fa8a79984f2e1ebf3524a
goetzsabrina <sabrina.goetz@hs-furtwangen.de>
2020-01-08 21:54:29 +0100

    data protection guidelines -not connected to navigation bar

---
+++ src/components/PopUp.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-dialog max-width="600">
+<v-btn flat slot="activator" class="success">Add new project<v/v-btn>
+    <v-card>
+        <v-card-title>
+            <h2>Datenschutzrichtlinien</h2>
+        </v-card-title>
+        //content
+    </v-card>
+
+
+
+
+        <p</v-dialog>
+</template>
+
+<script>
+    export default {
+        name: "PopUp"
+    }
+</script>
+
+<style scoped>
+
+</style>
