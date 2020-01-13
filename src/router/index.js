19681ba1b8e33a8d2348cc3a62703b90e837433a
lauramgr <56912363+lauramgr@users.noreply.github.com>
2020-01-13 13:22:10 +0100

    Router Update
    Message Me muss eingebunden werden, sowie alle Seiten
    
    Laura

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login'
-import App from '../App';
-import Login from '../App'
-import Startseite from '../App';
-import Profilseite from '../';
-import Profilseite Artist from '../';
-import Profilseite Studio from '../';
-import
 
-Vue.use(VueRouter)
+//import Profilseite from ".../views/Profilseite"
+//import Profilseite_Artist from ".../views/Profilseite_Artist"
+//import Profilseite_Studio from ".../views/Profilseite_Studio"
+//import Choose_your_Role from ".../views/Choose_your_Role"
+//import Datenschutzbestimmungen from ".../views/Datenschutzbestimmungen"
+//import Impressum from ".../views/Impressum"
+import Startseite from "../views/Startseite"
+
+
+Vue.use(VueRouter);
 
 export default new VueRouter({
     routes: [
         {
-            path: '/home',
-            component: Message.html
+            path: '/Startseite',
+            component: Startseite
+
         },
-        {
-            path: '/login',
-            component: Login
-        },
+
+       // {
+           // path: '/Profilseite.vue',
+            //component: Profilseite
+
+       // },
+       // {
+           // path: '/Profilseite_Artist.vue',
+           // component: Profilseite_Artist
+
+       // },
+       // {
+          // path: '/Profilseite_Studio.vue',
+            //component: Profilseite_Studio
+       // },
+        //{
+            //path: '/Choose_your_Role.vue',
+            //component: Choose_your_Role
+        //},
+        //{
+            //path: '/Datenschutzbestimmungen.vue',
+            //component: Datenschutzbestimmungen
+       // },
+        //{
+            //path: '/Impressum.vue',
+            //component: Impressum
+        //},
     ]
 })
